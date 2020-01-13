c98f313846c741e1fd3a463265219ba6500925ff
Leonardo Cardozo Ferreira <leonardo329cf@gmail.com>
2020-01-13 20:36:46 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/leonardocardozo/workshopmongo/controllers/UserController.java
+++ src/main/java/com/leonardocardozo/workshopmongo/controllers/UserController.java
@@ -45,4 +45,12 @@ public class UserController {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		userService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	
 }
