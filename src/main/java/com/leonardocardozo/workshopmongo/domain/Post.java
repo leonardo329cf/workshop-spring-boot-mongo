8f4ad278f1927404c65a203118c6fdf422052625
Leonardo Cardozo Ferreira <leonardo329cf@gmail.com>
2020-01-13 22:07:02 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/leonardocardozo/workshopmongo/domain/Post.java
+++ src/main/java/com/leonardocardozo/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.leonardocardozo.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.leonardocardozo.workshopmongo.dto.AuthorDTO;
+import com.leonardocardozo.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -19,6 +22,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	
 	public Post() {
@@ -74,6 +79,15 @@ public class Post implements Serializable {
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
 	
 	@Override
 	public int hashCode() {
@@ -99,6 +113,4 @@ public class Post implements Serializable {
 			return false;
 		return true;
 	}
-
-	
 }
